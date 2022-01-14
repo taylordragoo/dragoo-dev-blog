20c7bd8b4eeafc55d4fcfb1d93f9dfde90535af6
taylordragoo <taylor.dragoo@gmail.com>
2022-01-14 02:47:48 +0000

    changed footer message

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,6 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
-          <RiHeart2Line />
-        </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
       </p>
     </div>
   </footer>
